c8c8c2083c063f79bf4c89058aab075a0e79ca42
dinar <xewelus@gmail.com>
2019-11-22 16:37:56 +0300

    Трей

--- DexpBugDetectorWpf/App.xaml.cs
+++ DexpBugDetectorWpf/App.xaml.cs
@@ -1,17 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
 
 namespace DexpBugDetectorWpf
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
+		private readonly NotifyIcon notifyIcon = new NotifyIcon();
+		public App()
+		{
+			this.notifyIcon.Icon = DexpBugDetectorWpf.Properties.Resources.tv;
+			this.notifyIcon.Visible = true;
+			this.notifyIcon.Text = "DexpBugDetectorWpf";
+
+			this.notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+			ToolStripItem quitItem = this.notifyIcon.ContextMenuStrip.Items.Add("Выйти");
+			quitItem.Click += this.quitItem_Click;
+		}
+
+		private void quitItem_Click(object sender, EventArgs eventArgs)
+		{
+			this.Shutdown();
+		}
 	}
 }
